d24fa85d3544abaa96633dc8e63a14fb5a2f62ae
Diana Martinez Gonzalez <diana.martinezgonzalez@finsa.it>
2014-07-11 15:40:08 +0200

    Footer

--- FLEX.Web/FLEX/UserControls/PageFooter.ascx.cs
+++ FLEX.Web/FLEX/UserControls/PageFooter.ascx.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Web;
 using System.Web.UI;
 
 // ReSharper disable CheckNamespace
@@ -11,6 +12,8 @@ namespace FLEX.Web.UserControls
   {
     protected void Page_Load(object sender, EventArgs e)
     {
+      
+       lblHost.Text = "Host : "+HttpContext.Current.Server.MachineName;
     }
   }
 }
--- FLEX.Web/FLEX/UserControls/PageFooter.ascx.designer.cs
+++ FLEX.Web/FLEX/UserControls/PageFooter.ascx.designer.cs
@@ -11,5 +11,14 @@ namespace FLEX.Web.UserControls {
     
     
     public partial class PageFooter {
+        
+        /// <summary>
+        /// lblHost control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblHost;
     }
 }
