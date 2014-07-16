50ac2f9f2be6edb48b577e3399f4b1b63adf1d23
Alessio Parma <alessio.parma@gmail.com>
2014-07-16 12:26:30 +0200

    Bump to 0.9.4

--- FLEX.Web/Properties/AssemblyInfo.cs
+++ FLEX.Web/Properties/AssemblyInfo.cs
@@ -37,8 +37,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.9.4.*")]
+[assembly: AssemblyFileVersion("0.9.4.*")]
 
 // Attributes added to allow safer development.
 
