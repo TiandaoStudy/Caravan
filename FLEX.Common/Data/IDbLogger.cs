dac4a75a7a92becf09bdc9695ed8e9d89d9896ff
Alessio Parma <alessio.parma@gmail.com>
2014-07-28 14:42:15 +0200

    come prima

---
+++ FLEX.Common/Data/IDbLogger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FLEX.Common.Data
+{
+   public interface IDbLogger
+   {
+      void LogInfo<TFrom>(string infoMessage);
+   }
+}
---
+++ FLEX.Data.Oracle/OracleDbLogger.vb
@@ -0,0 +1,9 @@
+﻿Imports FLEX.Common.Data
+
+NotInheritable Class OracleDbLogger
+   Implements IDbLogger
+
+   Sub LogInfo(Of TFrom)(infoMessage As String) Implements IDbLogger.LogInfo
+      Throw New NotImplementedException()
+   End Sub
+End Class
