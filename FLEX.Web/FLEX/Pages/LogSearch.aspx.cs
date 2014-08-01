9de1d1ad89ea9a87889e3ea1be059f255d849cdc
Alessio Parma <alessio.parma@gmail.com>
2014-08-01 17:31:49 +0200

    catch

--- FLEX.Web/FLEX/Pages/LogSearch.aspx.cs
+++ FLEX.Web/FLEX/Pages/LogSearch.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using FLEX.Common;
+using FLEX.Web.UserControls.Ajax;
 
 // ReSharper disable CheckNamespace
 // This is the correct namespace, despite the file physical position.
@@ -11,17 +12,35 @@ namespace FLEX.Web.Pages
    {
       protected void Page_Load(object sender, EventArgs e)
       {
-         if (!IsPostBack)
+         try
          {
-            fdtgLogs.UpdateDataSource();
+            if (!IsPostBack)
+            {
+               fdtgLogs.UpdateDataSource();
+            }
          }
-         
-         btnRefresh.Click += (s, a) => fdtgLogs.UpdateDataSource();
+         catch (Exception ex)
+         {
+            ErrorHandler.CatchException(ex, ErrorLocation.PageEvent);
+         }
       }
 
       protected void fdtgLogs_DataSourceUpdating(object sender, EventArgs args)
       {
+         // This should not catch any exception, others will do.
          fdtgLogs.DataSource = DbLogger.Instance.RetrieveCurrentApplicationLogsTable();
+      }
+
+      protected void btnRefresh_Click(object sender, EventArgs args)
+      {
+         try
+         {
+            fdtgLogs.UpdateDataSource();
+         }
+         catch (Exception ex)
+         {
+            ErrorHandler.CatchException(ex);
+         }
       }
    }
 }
