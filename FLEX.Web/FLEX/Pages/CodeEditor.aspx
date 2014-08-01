d748b92674828ca54b39622826e363e6c4d213fc
Diana Martinez Gonzalez <diana.martinezgonzalez@finsa.it>
2014-08-01 12:14:58 +0200

    ...

---
+++ FLEX.Web/FLEX/Pages/CodeEditor.aspx
@@ -0,0 +1,80 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CodeEditor.aspx.cs" Inherits="FLEX.Web.Pages.CodeEditor" MasterPageFile="~/FLEX/MasterPages/CustomView.Master" %>
+<%@ MasterType VirtualPath="~/FLEX/MasterPages/CustomView.Master"%>
+<%@ Register TagPrefix="ajax" Namespace="System.Web.UI" Assembly="System.Web.Extensions" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="headContent" runat="server">
+   <title>Files</title>  
+
+<style type="text/css" media="screen">
+    #editor { 
+        /*position: absolute;*/
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        height:400px;
+    }
+</style>
+
+   <script src="FLEX/Scripts/ace/ace.js" type="text/javascript" charset="utf-8"></script>
+
+<script>
+
+
+   function ChangeModeEditor(mode) {
+      var editor = ace.edit("editor");
+      editor.setTheme("ace/theme/eclipse");
+      switch (mode) {
+         case 'cs':
+            editor.getSession().setMode("ace/mode/csharp");
+            break;
+         case 'xml':
+            editor.getSession().setMode("ace/mode/xml");
+            break;
+         default:
+            editor.getSession().setMode("ace/mode/csharp");
+      }
+      var textbox = $("#<%= txtContentFiles.ClientID %>").hide();
+      editor.getSession().setValue(textbox.val());
+   }
+
+   function Save() {
+      var editor = ace.edit("editor");
+      var text = editor.getSession().getValue();
+      $("#<%= txtContentFiles.ClientID %>").val(text);
+      }
+   </script>
+
+</asp:Content>
+
+<asp:Content ID="aspCustomContent" ContentPlaceHolderID="customContent" runat="server">
+    <div class="col-xs-12">
+         <ajax:UpdatePanel ID="UpdatePanel3" runat="server">
+            <ContentTemplate>
+               <div class="panel-body">                    
+                  <div class="row">
+                      <div class="col-xs-4">
+                            <asp:TreeView ID="TreeView1" runat="server" 
+                             onselectednodechanged="TreeView1_SelectedNodeChanged" OnTreeNodeExpanded="TreeView1_TreeNodeExpanded" OnTreeNodeCollapsed="TreeView1_TreeNodeCollapsed">
+                            </asp:TreeView>
+                      </div>
+                      <div class="col-xs-8">
+                            <div class="row" runat="server" id="divContenFiles">
+                                    <div id="editor">
+                                    </div>
+                                    <asp:TextBox runat="server" ID="txtContentFiles" Visible="false" TextMode="MultiLine" ></asp:TextBox>
+                         </div>
+                           <div class="row text-right">
+                               <asp:LinkButton class="btn btn-success btn-sm" ID="lbtnSave" Visible="false"  runat="server" OnClick="lbtnSave_Click" OnClientClick="Save();"><span class="glyphicon glyphicon-floppy-disk"></span>&nbsp;Save</asp:LinkButton>
+                           </div>
+                       </div>
+                  
+                </div>
+             </div>
+           </ContentTemplate>
+         </ajax:UpdatePanel>
+      </div>
+
+
+
+</asp:Content>
---
+++ FLEX.Web/FLEX/Pages/CodeEditor.aspx.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Web.UI.WebControls;
+using FLEX.Common;
+using FLEX.Web.Pages;
+
+namespace FLEX.Web.Pages
+{
+   public partial class CodeEditor : PageBase
+   {
+      Dictionary<string, string> FilesPath = new Dictionary<string, string>();
+      protected void Page_Load(object sender, EventArgs e)
+      {
+         if (!Page.IsPostBack)
+         {
+            ViewState["dictFilesPath"] = FilesPath;
+            var scriptsPath = Armando.Configuration.Instance.ScriptsPath;
+            var xmlPath = Server.MapPath(FLEX.Web.WebSettings.AjaxLookup_XmlPath);
+            var menuPath = Server.MapPath(FLEX.Web.WebSettings.MenuBar_XmlPath);
+            Dictionary<string, string> _Files = new Dictionary<string, string>();
+            _Files.Add("Data Scritps", scriptsPath);
+            _Files.Add("Ajax Lookup", xmlPath);
+            _Files.Add("Menu", menuPath);
+            BuildTreeView(_Files);
+         }
+      }
+
+
+      protected void TreeView1_SelectedNodeChanged(object sender, EventArgs e)
+      {
+         if (TreeView1.SelectedNode.Text == "Data Scritps" || TreeView1.SelectedNode.Text == "Ajax Lookup" || TreeView1.SelectedNode.Text == "Menu")
+         {
+            txtContentFiles.Text = "";
+            lbtnSave.Visible = false;
+            txtContentFiles.Visible = false;
+            divContenFiles.Visible = false;
+            return;
+         }
+         else
+         {
+            divContenFiles.Visible = true;
+            lbtnSave.Visible = true;
+            txtContentFiles.Visible = true;
+            Dictionary<string, string> _FilesPath = (Dictionary<string, string>)ViewState["dictFilesPath"];
+
+            var _name_files = TreeView1.SelectedNode.Text;
+            string _text;
+            using (StreamReader reader = new StreamReader(_FilesPath[_name_files]))
+            {
+               _text = reader.ReadToEnd();
+            }
+            txtContentFiles.Text = _text;
+            string _extension = Path.GetExtension(_FilesPath[_name_files]);
+            if (_extension == ".cs")
+               System.Web.UI.ScriptManager.RegisterStartupScript(this, this.GetType(), "ChangeModeEditor", "ChangeModeEditor('cs');", true);
+            else if (_extension == ".xml")
+               System.Web.UI.ScriptManager.RegisterStartupScript(this, this.GetType(), "ChangeModeEditor", "ChangeModeEditor('xml');", true);
+
+         }
+      }
+
+      public void BuildTreeView(Dictionary<string, string> _Files)
+      {
+         int index = 0;
+         foreach (var item in _Files)
+         {
+            TreeView1.Nodes.Add(new TreeNode(item.Key));
+            if (Path.GetExtension(item.Value) == "")
+            {
+               string[] files = Directory.GetFiles(item.Value);
+               for (int i = 0; i < files.Length; i++)
+               {
+                  TreeView1.Nodes[index].ChildNodes.Add(new TreeNode(Path.GetFileName(files[i])));
+                  FilesPath.Add(Path.GetFileName(files[i]), files[i]);
+               }
+               index++;
+            }
+            else
+            {
+               TreeView1.Nodes[index].ChildNodes.Add(new TreeNode(Path.GetFileName(item.Value)));
+               FilesPath.Add(Path.GetFileName(item.Value), item.Value);
+            }
+         }
+         TreeView1.DataBind();
+      }
+
+      protected void lbtnSave_Click(object sender, EventArgs e)
+      {
+         divContenFiles.Visible = true;
+         Dictionary<string, string> _FilesPath = (Dictionary<string, string>)ViewState["dictFilesPath"];
+
+         var _name_files = TreeView1.SelectedNode.Text;
+
+         using (StreamWriter writer = new StreamWriter(_FilesPath[_name_files], false))
+         {
+            writer.Write(txtContentFiles.Text);
+         }
+
+         var logMsg = String.Format("Data script {0} has been recompiled", _FilesPath[_name_files]);
+         DbLogger.Instance.LogWarning<CodeEditor>("lbtnSave_Click", logMsg);
+
+         string _extension = Path.GetExtension(_FilesPath[_name_files]);
+         if (_extension == ".cs")
+            System.Web.UI.ScriptManager.RegisterStartupScript(this, this.GetType(), "ChangeModeEditor", "ChangeModeEditor('cs');", true);
+         else if (_extension == ".xml")
+            System.Web.UI.ScriptManager.RegisterStartupScript(this, this.GetType(), "ChangeModeEditor", "ChangeModeEditor('xml');", true);
+
+
+      }
+
+      protected void TreeView1_TreeNodeExpanded(object sender, TreeNodeEventArgs e)
+      {
+         lbtnSave.Visible = false;
+         divContenFiles.Visible = false;
+      }
+
+      protected void TreeView1_TreeNodeCollapsed(object sender, TreeNodeEventArgs e)
+      {
+         lbtnSave.Visible = false;
+         divContenFiles.Visible = false;
+      }
+
+
+   }
+}
