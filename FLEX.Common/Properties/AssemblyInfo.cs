2fd76b8092eb6fdf4f4f4625fc2198f0e0feee69
Alessio Parma <alessio.parma@gmail.com>
2014-08-04 08:56:39 +0200

    bump to 0.9.18

--- FLEX.Common/Properties/AssemblyInfo.cs
+++ FLEX.Common/Properties/AssemblyInfo.cs
@@ -37,8 +37,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.9.17.0")]
-[assembly: AssemblyFileVersion("0.9.17.0")]
+[assembly: AssemblyVersion("0.9.18.0")]
+[assembly: AssemblyFileVersion("0.9.18.0")]
 
 // Attributes added to allow stronger development.
 
