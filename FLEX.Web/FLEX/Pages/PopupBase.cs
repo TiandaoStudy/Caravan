a0a2a74a82b15946a365dc1270a48fe931a78488
Alessio Parma <alessio.parma@gmail.com>
2014-08-07 10:58:43 +0200

    template per popup

---
+++ FLEX.Web/FLEX/Pages/PopupBase.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using FLEX.Web.MasterPages;
+
+// ReSharper disable CheckNamespace
+// This is the correct namespace, despite the file physical position.
+
+namespace FLEX.Web.Pages
+// ReSharper restore CheckNamespace
+{
+   public abstract class PopupBase : Page, IPopupBase
+   {
+      #region IPopupBase Members
+
+      public UserControls.Ajax.ErrorHandler ErrorHandler
+      {
+         get { return MasterPopup.ErrorHandler; }
+      }
+
+      public HtmlForm MainForm
+      {
+         get { return MasterPopup.MainForm; }
+      }
+      
+      public void RegisterCloseScript(Page child)
+      {
+         MasterPopup.RegisterCloseScript(this);
+      }
+
+      #endregion
+
+      #region Private Members
+
+      private IPopupBase MasterPopup
+      {
+         get
+         {
+            Debug.Assert(Master is IPopupBase);
+            return Master as IPopupBase;
+         }
+      }
+
+      #endregion
+   }
+}
