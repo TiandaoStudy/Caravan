d4e9df48c12f7a0882e7fff51076f2841e695c9a
Alessio Parma <alessio.parma@gmail.com>
2014-08-17 10:14:54 +0200

    vacuum per kvlite

--- FLEX.Web/GlobalHelper.cs
+++ FLEX.Web/GlobalHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Web;
 using FLEX.Common.Data;
+using KVLite;
 
 namespace FLEX.Web
 {
@@ -9,6 +10,16 @@ namespace FLEX.Web
       private GlobalHelper()
       {
          throw new InvalidOperationException();
+      }
+      
+      public static void Application_Start(object sender, EventArgs args, string connectionString)
+      {
+         // Sets the default connection string.
+         Common.Configuration.Instance.ConnectionString = connectionString;
+
+         // Run vacuum on the persistent cache. It should be put AFTER the connection string is set,
+         // since that string it stored on the cache itself and we do not want conflicts, right?
+         PersistentCache.DefaultInstance.VacuumAsync();
       }
 
       public static void Application_Error(object sender, EventArgs args)
