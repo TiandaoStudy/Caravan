b946d9ccf6e3f9ec45b8fb80da104f6ede9d824f
Alessio Parma <alessio.parma@gmail.com>
2014-10-04 10:05:26 +0200

    semplificazione di common

---
+++ FLEX.Web.WebAPI/Results/BsonNetResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FLEX.Web.WebAPI.Results
+{
+    class BsonNetResult
+    {
+    }
+}
