1c17250f7eaf633f59fb48ec0d0eb0516b74af7d
Alessio Parma <alessio.parma@gmail.com>
2014-10-07 11:07:36 +0200

    rilascio webforms

--- FLEX.Web/Properties/AssemblyInfo.cs
+++ FLEX.Web/Properties/AssemblyInfo.cs
@@ -37,8 +37,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.13.0.2")]
-[assembly: AssemblyFileVersion("0.13.0.2")]
+[assembly: AssemblyVersion("0.14.0.1")]
+[assembly: AssemblyFileVersion("0.14.0.1")]
 
 // Attributes added to allow safer development.
 
