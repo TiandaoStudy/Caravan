8e7b7ef6bd778b0717a88c0dd586e0baddf331d6
Sara Viacava <sara82v@users.noreply.github.com>
2014-11-04 14:56:10 +0100

    ...

--- UnitTests.DataAccess/SecurityManagerTests.cs
+++ UnitTests.DataAccess/SecurityManagerTests.cs
@@ -2,10 +2,72 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NUnit.Framework;
 
 namespace UnitTests.DataAccess
 {
+    
+    [TestFixture]
     class SecurityManagerTests
     {
+        public SecurityManagerTests()
+        {
+            
+        }
+
+        [SetUp]
+        public void Init()
+        {
+
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+
+        }
+        
+        [Test]
+        public void Users_Insert2Users_ReturnsListOfUsers()
+        {
+            
+        }
+
+        [Test]
+        public void Users_NoUsers_ReturnsNull()
+        {
+
+        }
+
+        [Test]
+        public void Users_NoUsers_ReturnNull()
+        {
+
+        }
+
+        [Test]
+        public void User_UserPresent_ReturnsUser()
+        {
+
+        }
+
+        [Test]
+        public void User_UserNotPresent_ReturnsNull()
+        {
+
+        }
+
+        [Test]
+        public void AddUser_InsertUser_InsertOk()
+        {
+
+        }
+
+
+
+
+        
+
+        
     }
 }
