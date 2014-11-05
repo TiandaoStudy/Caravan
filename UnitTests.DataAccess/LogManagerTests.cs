2721e4abeb440964519ba3e66f30cef7baf03e40
Sara Viacava <sara82v@users.noreply.github.com>
2014-11-05 12:40:28 +0100

    ...

--- UnitTests.DataAccess/LogManagerTests.cs
+++ UnitTests.DataAccess/LogManagerTests.cs
@@ -9,6 +9,43 @@ namespace UnitTests.DataAccess
     [TestFixture]
     public class LogManagerTests
     {
+        
+        [Test]
+        public void LogSettings_NullArg_ReturnListOfAllApps()
+        {
 
+        }
+        [Test]
+        public void LogSettings_ValidAppName_ReturnsListForAppName(){
+
+        }
+
+        [Test]
+        public void LogSettings_ValidArgs_Returns()
+        {
+
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void LogSettings_NullLogTypeAndValidAppName_ThrowsArgumentNullException()
+        {
+
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void LogSettings_NullAppNameValidLogType_ThrowsArgumentNullException()
+        {
+
+        }
+
+        //[Test]
+        //[ExpectedException(typeof(ArgumentException))]
+        //public void LogSettings_NotExistingAppName_ThrowsArgumenException()
+        //{
+
+        //}
+                
     }
 }
--- UnitTests.DataAccess/SecurityManagerTests.cs
+++ UnitTests.DataAccess/SecurityManagerTests.cs
@@ -6,13 +6,13 @@ using NUnit.Framework;
 
 namespace UnitTests.DataAccess
 {
-    
+
     [TestFixture]
     class SecurityManagerTests
     {
         public SecurityManagerTests()
         {
-            
+
         }
 
         [SetUp]
@@ -26,10 +26,28 @@ namespace UnitTests.DataAccess
         {
 
         }
-        
+
+        #region Users_Tests
+
         [Test]
         public void Users_Insert2Users_ReturnsListOfUsers()
         {
+            string name_user1= "pippo";
+            string login_user1 = "blabla1";
+            string name_user2 = "pluto";
+            string login_user2 = "blabla2";
+            string appName = "mio_test";
+
+            //creare 2 oggetti SecUser
+            
+            //creare App con "appName"
+            
+            //chiamare AddUser passando i parametri creati
+            //AddUser(appName,)
+            
+            //richiamare il metodo da testare (Users)
+            //IEnumerable<SecUser> retValue = Users(appName);
+            //Assert.That(retValue.Count(),IsEqualTo(2));
             
         }
 
@@ -40,34 +58,134 @@ namespace UnitTests.DataAccess
         }
 
         [Test]
-        public void Users_NoUsers_ReturnNull()
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Users_NullAppName_ThrowsArgumentNullException()
+        {
+
+        }
+
+        //[Test]
+
+        //public void Users_NotExistingAppName_ThrowsArgumentException()
+        //{
+
+        //}
+
+        #endregion
+
+        #region User_Tests
+        [Test]
+        public void User_ValidArgs_ReturnsUser()
         {
 
         }
 
         [Test]
-        public void User_UserPresent_ReturnsUser()
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void User_NullAppName_ThrowsArgumentNullException()
+        {
+
+        }
+
+        //[Test]
+        //[ExpectedException(typeof(ArgumentException))]
+        //public void User_NotExistingAppName_ThrowsArgumentException()
+        //{
+
+        //}
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void User_NullUserLogin_ThrowsArgumentNullException()
+        {
+
+        }
+
+        #endregion
+
+        #region AddUser_Tests
+
+        [Test]
+        public void AddUser_ValidArgs_InsertOk()
         {
 
         }
 
         [Test]
-        public void User_UserNotPresent_ReturnsNull()
+        [ExpectedException]//inserire il tipo di eccezione aspettata
+        public void AddUser_UserAlreadyPresent_ThrowsException()
         {
 
         }
 
         [Test]
-        public void AddUser_InsertUser_InsertOk()
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void AddUser_NullAppName_ThrowsArgymentNullException()
         {
 
         }
 
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void AddUser_NullNewUser_ThrowsArgymentNullException()
+        {
 
+        }
 
+        #endregion
 
-        
+        #region RemoveUser_Tests
 
-        
+        [Test]
+        public void RemoveUser_ValidArgs_RemoveUser()
+        {
+
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void RemoveUser_NullAppName_ThrowsArgumentNullException()
+        {
+
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void RemoveUser_NullUserLogin_ThrowsArgumentNullException(){
+
+        }
+
+        #endregion
+
+        #region UpdateUser_Tests
+
+        [Test]
+        public void UpdateUser_ValidArgs_UserUpdated()
+        {
+
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void UpdateUser_NullAppName_ThrowsArgumentNullException()
+        {
+
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void UpdateUser_NullUserLogin_ThrowsArgumentNullException()
+        {
+
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void UpdateUser_NullNewUserArg_ThrowsArgumentNullException()
+        {
+
+        }
+
+        #endregion
     }
 }
