57ab1392f1e06d9a327a56dc2a6f518401cb0cb0
Alessio Parma <alessio.parma@gmail.com>
2014-11-11 11:38:15 +0100

    aggirato bug su Chrome

---
+++ Caravan.WebForms/FLEX/UserControls/PopupExitButton.ascx.cs
@@ -0,0 +1,19 @@
+﻿// ReSharper disable CheckNamespace
+// This is the correct namespace, despite the file physical position.
+
+namespace FLEX.Web.UserControls
+// ReSharper restore CheckNamespace
+{
+   public partial class PopupExitButton : ControlBase
+   {
+      #region Public Properties
+
+      public string Text
+      {
+         get { return lblText.Text; }
+         set { lblText.Text = value; }
+      }
+
+      #endregion
+   }
+}
---
+++ Sample.WebUI.WebForms/EmptyPage.aspx
@@ -0,0 +1,1 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="EmptyPage.aspx.cs" Inherits="FLEX.Sample.WebUI.EmptyPage" %>
