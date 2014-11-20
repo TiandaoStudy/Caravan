0156019feb8eea9b22a62cd5599205b998dd76e9
Alessio Parma <alessio.parma@gmail.com>
2014-11-20 17:33:33 +0100

    varie...

---
+++ Caravan.Common/RestService/IAuthManager.cs
@@ -0,0 +1,14 @@
+﻿namespace Finsa.Caravan.RestService
+{
+   public interface IAuthManager
+   {
+      AuthResult Authenticate(dynamic authObject);
+   }
+
+   public struct AuthResult
+   {
+      public bool IsValid { get; set; }
+
+      public string Message { get; set; }
+   }
+}
