b9d3fe2d8b16d432497fb2e4cb52f5696dc3fb80
Alessio Parma <alessio.parma@gmail.com>
2014-11-20 12:20:38 +0100

    varie su newage

---
+++ Caravan.Common/DataModel/Security/SessionTracker.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+
+namespace Finsa.Caravan.DataModel.Security
+{
+    public class SessionTracker
+    {
+        private string _userHostName;
+        private string _userHostAddress;
+        private string _userName;
+        private DateTime _lastVisit;
+
+        public string UserHostAddress
+        {
+            get
+            {
+                return this._userHostAddress;
+            }
+        }
+        public string UserHostName
+        {
+            get
+            {
+                return this._userHostName;
+            }
+        }
+        public DateTime LastVisit
+        {
+            get
+            {
+                return this._lastVisit;
+            }
+            set
+            {
+                this._lastVisit = value;
+            }
+        }
+        public string Login
+        {
+            get
+            {
+                return this._userName;
+            }
+            set
+            {
+                this._userName = value;
+            }
+        }
+
+        public void FillData() 
+        {
+       
+            this._userHostAddress = HttpContext.Current.Request.UserHostAddress;
+            if (this._userHostAddress == "::1")
+            {
+                this._userHostAddress = "localhost";
+            }
+        
+            this._userHostName = HttpContext.Current.Request.UserHostName;
+            if (this._userHostName == "::1")
+            {
+                this._userHostName = "localhost";
+            }
+
+            this._lastVisit = DateTime.Now;
+        }
+    }
+}
