dbcf740c59895f243a8298cd2ca2fd4c2d40143a
Alessio Parma <alessio.parma@gmail.com>
2014-11-20 12:54:08 +0100

    correzioni

---
+++ Caravan.WebForms/FLEX/MasterPages/IPopup.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.UI;
+
+// ReSharper disable CheckNamespace
+// This is the correct namespace, despite the file physical position.
+
+namespace FLEX.Web.MasterPages
+// ReSharper restore CheckNamespace
+{
+   /// <summary>
+   ///   TODO
+   /// </summary>
+   public interface IPopup : IHead
+   {
+      /// <summary>
+      ///   TODO
+      /// </summary>
+      /// <param name="child"></param>
+      /// <param name="message"></param>
+      void RegisterAlert(Page child, string message);
+
+      /// <summary>
+      ///   TODO
+      /// </summary>
+      /// <param name="child"></param>
+      void RegisterCloseScript(Page child);
+   }
+}
