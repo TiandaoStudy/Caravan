6847623138e2d56a6081780ad5e4370024db0c2c
Sara Viacava <sara82v@users.noreply.github.com>
2015-01-12 09:56:28 +0100

    end tests

---
+++ Caravan.DataAccess/IDbManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Data.Common;
+
+namespace Finsa.Caravan.DataAccess
+{
+   /// <summary>
+   /// 
+   /// </summary>
+   public interface IDbManager
+   {
+      DataAccessKind Kind { get; }
+
+      /// <summary>
+      /// 
+      /// </summary>
+      /// <param name="connectionString"></param>
+      void ElaborateConnectionString(ref string connectionString);
+
+      /// <summary>
+      /// 
+      /// </summary>
+      /// <returns></returns>
+      DbConnection OpenConnection();
+
+      /// <summary>
+      /// 
+      /// </summary>
+      /// <returns></returns>
+      DbConnection CreateConnection();
+   }
+
+   public enum DataAccessKind : byte
+   {
+      Oracle = 0,
+      Postgres = 1,
+      Rest = 2,
+      SqlServer = 3,
+      SqlServerCe = 4
+   }
+}
