faee67ed213ff7970baf5d0df92a0b3fb1218002
Alessio Parma <alessio.parma@gmail.com>
2015-01-19 13:49:23 +0100

    serializzatore mod.

--- Caravan.WebService/Controllers/LoggerController.cs
+++ Caravan.WebService/Controllers/LoggerController.cs
@@ -1,21 +1,21 @@
-﻿using System.Collections.Generic;
-using System.Web.Http;
-using Finsa.Caravan.Common.DataModel.Logging;
+﻿using Finsa.Caravan.Common.DataModel.Logging;
 using Finsa.Caravan.DataAccess;
+using System.Collections.Generic;
+using System.Web.Http;
 
 namespace Caravan.WebService.Controllers
 {
-    [RoutePrefix("logger")]
-    public class LoggerController : ApiController
-    {
-        /// <summary>
-        ///   Returns all log entries.
-        /// </summary>
-        /// <returns>All log entries.</returns>
-        [Route("entries")]
-        public IList<LogEntry> GetLogEntries()
-        {
-            return Db.Logger.Logs();
-        }
-    }
+   [RoutePrefix("logger")]
+   public class LoggerController : ApiController
+   {
+      /// <summary>
+      ///   Returns all log entries.
+      /// </summary>
+      /// <returns>All log entries.</returns>
+      [Route("entries")]
+      public IList<LogEntry> GetLogEntries()
+      {
+         return Db.Logger.Logs();
+      }
+   }
 }
