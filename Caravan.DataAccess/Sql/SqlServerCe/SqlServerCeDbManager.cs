a6ae3ef57d7fda3e5d920469fd34bef34bd9fff5
Alessio Parma <alessio.parma@gmail.com>
2015-01-19 08:47:28 +0100

    Merge with dev-alessiop

---
+++ Caravan.DataAccess/Sql/SqlServerCe/SqlServerCeDbManager.cs
@@ -0,0 +1,28 @@
+using System.Data.Common;
+using Finsa.Caravan.DataAccess.Core;
+using Finsa.Caravan.DataAccess.Properties;
+
+namespace Finsa.Caravan.DataAccess.Sql.SqlServerCe
+{
+	public sealed class SqlServerCeDbManager : DbManagerBase
+	{
+		private static readonly DbProviderFactory DbFactory = DbProviderFactories.GetFactory("System.Data.SqlServerCe.4.0");
+
+	   public override DataAccessKind Kind
+	   {
+	      get { return DataAccessKind.SqlServerCe; }
+	   }
+
+	   public override void ElaborateConnectionString(ref string connectionString)
+		{
+			connectionString = connectionString;
+		}
+
+		public override DbConnection CreateConnection()
+		{
+			var connection = DbFactory.CreateConnection();
+			connection.ConnectionString = Db.ConnectionString;
+			return connection;
+		}
+	}
+}
---
+++ Caravan.WebForms/FLEX/MasterPages/IPage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using FLEX.Web.UserControls;
+
+// ReSharper disable CheckNamespace
+// This is the correct namespace, despite the file physical position.
+
+namespace FLEX.Web.MasterPages
+// ReSharper restore CheckNamespace
+{
+   /// <summary>
+   ///   TODO
+   /// </summary>
+   public interface IPage : IHead
+   {
+      /// <summary>
+      ///   TODO
+      /// </summary>
+      bool HasPageVisibleHandlers { get; }
+
+      /// <summary>
+      ///   TODO
+      /// </summary>
+      MenuBar MenuBar { get; }
+
+      /// <summary>
+      ///   TODO
+      /// </summary>
+      PageFooter PageFooter { get; }
+
+      /// <summary>
+      ///   TODO
+      /// </summary>
+      event EventHandler Page_Visible;
+   }
+}
