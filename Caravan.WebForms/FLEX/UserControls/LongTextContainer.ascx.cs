74122293a4d82c61fee9367000ecd17b0fe4f5c2
Alessio Parma <alessio.parma@gmail.com>
2015-01-29 15:18:22 +0100

    clipboard per long txt

--- Caravan.WebForms/FLEX/UserControls/LongTextContainer.ascx.cs
+++ Caravan.WebForms/FLEX/UserControls/LongTextContainer.ascx.cs
@@ -1,43 +1,48 @@
-﻿using System;
-using PommaLabs.Diagnostics;
+﻿using PommaLabs.Diagnostics;
+using System;
+using PommaLabs.Extensions;
 
-// ReSharper disable CheckNamespace
-// This is the correct namespace, despite the file physical position.
+// ReSharper disable CheckNamespace This is the correct namespace, despite the file physical position.
 
 namespace FLEX.Web.UserControls
 // ReSharper restore CheckNamespace
 {
-   public partial class LongTextContainer : ControlBase
-   {
-      private const string AAAAA = "LongTextContainer.Text";
+    public partial class LongTextContainer : ControlBase
+    {
+        private const string TextViewStateKey = "LongTextContainer.Text";
 
-      #region Public Properties
+        #region Public Properties
 
-      public string ContainerTitle { get; set; }
+        public string ContainerTitle { get; set; }
 
-      public int MaxTextLength { get; set; }
+        public int MaxTextLength { get; set; }
 
-      public string ShortenedText { get; private set; }
+        public string ShortenedText { get; private set; }
 
-      public string Text
-      {
-         get { return (string) (ViewState[AAAAA] ?? ""); }
-         set
-         {
-            Raise<ArgumentNullException>.IfIsNull(value, "Text property cannot be null");           
-            if (value.Length <= MaxTextLength)
+        public string Text
+        {
+            get { return (string) (ViewState[TextViewStateKey] ?? ""); }
+            set
             {
-               // Nothing to do, text fits the container.
-               ShortenedText = value.Replace(Environment.NewLine, "");
+                Raise<ArgumentNullException>.IfIsNull(value, "Text property cannot be null");
+                if (value.Length <= MaxTextLength)
+                {
+                    // Nothing to do, text fits the container.
+                    ShortenedText = value.Replace(Environment.NewLine, "");
+                }
+                else
+                {
+                    ShortenedText = value.Substring(0, MaxTextLength).Replace(Environment.NewLine, "") + "...";
+                }
+                ViewState[TextViewStateKey] = value.Replace(Environment.NewLine, "<br/>"); // Text should not have normal line breaks (it is HTML).
             }
-            else
-            {
-               ShortenedText = value.Substring(0, MaxTextLength).Replace(Environment.NewLine, "") + "...";
-            }
-            ViewState[AAAAA] = value.Replace(Environment.NewLine, "<br/>"); // Text should not have normal line breaks (it is HTML).
-         }
-      }
+        }
 
-      #endregion
-   }
+        public string EscapedText
+        {
+            get { return Text.HtmlEncode().Replace(Environment.NewLine, "<br/>"); }
+        }
+
+        #endregion Public Properties
+    }
 }
--- Sample.WebUI.WebForms/FLEX/UserControls/LongTextContainer.ascx
+++ Sample.WebUI.WebForms/FLEX/UserControls/LongTextContainer.ascx
@@ -1,35 +1,34 @@
 ﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="LongTextContainer.ascx.cs" Inherits="FLEX.Web.UserControls.LongTextContainer" %>
 <%@ Import Namespace="PommaLabs.Extensions" %>
 
-<label id="shortenedText" class="long-text-ctn-label" runat="server"><%= Server.HtmlEncode(ShortenedText) %></label>
+<a href="#" onclick="window.prompt('Copy to clipboard: Ctrl+C, Enter', <%= Text.HtmlEncode().ToJavaScriptString(true) %>); return false;"><i class="fa fa-clipboard"></i></a>
+<label id="shortenedText" class="long-text-ctn-label" runat="server"><%= ShortenedText.HtmlEncode() %></label>
 
 <style type="text/css">
-   .popover {
-	   max-width: 50%;
-	   width: auto;
-   }
+    .popover {
+        max-width: 50%;
+        width: auto;
+    }
 </style>
 
 <script type="text/javascript">
-   $('#<%= shortenedText.ClientID %>').popover({
-      animation: true,
-      container: "body",
-      placement: "auto",
-      html: true,
-      trigger: "hover",
-      content: <%= Server.HtmlEncode(Text).ToJavaScriptString() %>,
-      title: <%= ContainerTitle.ToJavaScriptString() %>
-      }).one('click', function() {
-         $('#<%= shortenedText.ClientID %>').popover('destroy')
-         .popover({
-            animation: true,
-            container: "body",
-            placement: "auto",
-            html: true,
-            trigger: "click",
-            content: <%= Text.ToJavaScriptString() %>,
-            title: <%= ContainerTitle.ToJavaScriptString() %>
-            })
-         .popover('show');
-   });
+    $('#<%= shortenedText.ClientID %>').popover({
+        animation: true,
+        container: "body",
+        placement: "auto",
+        html: true,
+        trigger: "hover",
+        content: <%= Text.HtmlEncode().Replace(Environment.NewLine, "<br/>").ToJavaScriptString() %>,
+        title: <%= ContainerTitle.ToJavaScriptString() %>
+    }).one('click', function() {
+        $('#<%= shortenedText.ClientID %>').popover('destroy').popover({
+        animation: true,
+        container: "body",
+        placement: "auto",
+        html: true,
+        trigger: "click",
+        content: <%= Text.HtmlEncode().Replace(Environment.NewLine, "<br/>").ToJavaScriptString() %>,
+        title: <%= ContainerTitle.ToJavaScriptString() %>
+        }).popover('show');
+    });
 </script>
