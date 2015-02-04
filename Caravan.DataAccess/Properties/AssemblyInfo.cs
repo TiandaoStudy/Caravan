92712ee58fe0caa0ba2a5ea58a7b76111de366a8
Alessio Parma <alessio.parma@gmail.com>
2015-02-04 10:42:39 +0100

    v1.3.4

--- Caravan.DataAccess/Properties/AssemblyInfo.cs
+++ Caravan.DataAccess/Properties/AssemblyInfo.cs
@@ -37,8 +37,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")] 
 
-[assembly: AssemblyVersion("1.3.3")]
-[assembly: AssemblyFileVersion("1.3.3")]
+[assembly: AssemblyVersion("1.3.4")]
+[assembly: AssemblyFileVersion("1.3.4")]
 
 // Attributes added to allow stronger development.
 
