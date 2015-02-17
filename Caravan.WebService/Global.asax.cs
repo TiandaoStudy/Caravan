f2bb6dd4b8337ef236beff0e744e6941778036e9
Alessio Parma <alessio.parma@gmail.com>
2015-02-17 16:13:15 +0100

    pulizia web service

--- Caravan.WebService/Global.asax.cs
+++ Caravan.WebService/Global.asax.cs
@@ -2,24 +2,19 @@
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
-using System.Web.Optimization;
-using System.Web.Routing;
 
 namespace Caravan.WebService
 {
-   public class WebApiApplication : HttpApplication
-   {
-      protected void Application_Start()
-      {
-         // Inizializzatori di default.
-         AreaRegistration.RegisterAllAreas();
-         GlobalConfiguration.Configure(WebApiConfig.Register);
-         FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-         RouteConfig.RegisterRoutes(RouteTable.Routes);
-         BundleConfig.RegisterBundles(BundleTable.Bundles);
+    public class WebApiApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            // Inizializzatori di default.
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
 
-         // Inizializzatore per Caravan.
-         GlobalHelper.Application_Start();
-      }
-   }
+            // Inizializzatore per Caravan.
+            GlobalHelper.Application_Start();
+        }
+    }
 }
