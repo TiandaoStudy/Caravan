56610593fac826a33d0a5056c7daa3daacb17001
Alessio Parma <alessio.parma@gmail.com>
2015-02-22 11:23:19 +0100

    v1.5.0

--- Caravan.WebForms/Properties/AssemblyInfo.cs
+++ Caravan.WebForms/Properties/AssemblyInfo.cs
@@ -36,7 +36,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.4.5")]
-[assembly: AssemblyFileVersion("1.4.5")]
+[assembly: AssemblyVersion("1.5.0")]
+[assembly: AssemblyFileVersion("1.5.0")]
 
 [assembly: InternalsVisibleTo("UnitTests.WebForms")]
