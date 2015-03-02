5c990b208c32f24fb9482f21c2582665c716bb98
Alessio Parma <alessio.parma@gmail.com>
2015-03-02 16:48:48 +0100

    ...

--- Caravan.DataAccess/Drivers/Sql/Oracle/Scripts/grants.sql
+++ Caravan.DataAccess/Drivers/Sql/Oracle/Scripts/grants.sql
@@ -2,8 +2,8 @@
 -- REPLACE 'dst' WITH USER DB NAME
 
 -- Security
-grant select on src.caravan_sec_app to dst;
+grant select on src.crvn_sec_apps to dst;
 
 -- Log
-grant select on src.caravan_log_settings to dst;
-grant select, insert on src.caravan_log to dst;
+grant select on src.crvn_log_settings to dst;
+grant select, insert on src.crvn_log_entries to dst;
