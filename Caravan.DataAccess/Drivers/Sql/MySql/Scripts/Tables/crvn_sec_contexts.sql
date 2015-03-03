1ae032e393b39ce94affe29d98ef70090e8e9f67
Alessio Parma <alessio.parma@gmail.com>
2015-03-03 13:07:05 +0100

    schemi per mysql

--- Caravan.DataAccess/Drivers/Sql/MySql/Scripts/Tables/crvn_sec_contexts.sql
+++ Caravan.DataAccess/Drivers/Sql/MySql/Scripts/Tables/crvn_sec_contexts.sql
@@ -1,15 +1,14 @@
 ﻿-- REPLACE 'mydb' WITH DB NAME
 
-CREATE TABLE mydb.crvn_sec_contexts
-(
-     cctx_id          NUMBER          NOT NULL
-   , capp_id          NUMBER          NOT NULL 
-   , cctx_name        NVARCHAR2(32)   NOT NULL
-   , cctx_descr       NVARCHAR2(256)  NOT NULL
-   , CHECK (cctx_name = lower(cctx_name)) ENABLE
-   , CONSTRAINT pk_crvn_sec_contexts PRIMARY KEY (cctx_id) ENABLE
-   , CONSTRAINT uk_crvn_sec_contexts UNIQUE (capp_id, cctx_name) ENABLE   
-   , CONSTRAINT fk_crvnsecctxs_crvnsecapps FOREIGN KEY (capp_id) REFERENCES mydb.crvn_sec_apps (capp_id) ON DELETE CASCADE ENABLE
-);
-
-CREATE SEQUENCE mydb.crvn_sec_contexts_id;
+CREATE TABLE IF NOT EXISTS `mydb`.`crvn_sec_contexts` (
+  `cctx_id`       INT             NOT NULL AUTO_INCREMENT,
+  `capp_id`       INT             NOT NULL,
+  `cctx_name`     VARCHAR(256)    NOT NULL,
+  `cctx_descr`    VARCHAR(256)    NOT NULL,
+  PRIMARY KEY (`cctx_id`),
+  UNIQUE INDEX `uk_crvn_sec_contexts` (`cctx_name`(32) ASC, `capp_id` ASC),
+  CONSTRAINT `fk_crvnsecctxs_crvnsecapps`
+    FOREIGN KEY (`capp_id`)
+    REFERENCES `mydb`.`crvn_sec_apps` (`capp_id`)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION);
--- Caravan.DataAccess/Drivers/Sql/MySql/Scripts/Tables/crvn_sec_objects.sql
+++ Caravan.DataAccess/Drivers/Sql/MySql/Scripts/Tables/crvn_sec_objects.sql
@@ -1,16 +1,15 @@
 ﻿-- REPLACE 'mydb' WITH DB NAME
 
-CREATE TABLE mydb.crvn_sec_objects
-(
-     cobj_id          NUMBER          NOT NULL
-   , cctx_id          NUMBER          NOT NULL
-   , cobj_name        NVARCHAR2(32)   NOT NULL
-   , cobj_descr       NVARCHAR2(256)  NOT NULL
-   , cobj_type        NVARCHAR2(8)    NOT NULL
-   , CHECK (cobj_name = lower(cobj_name)) ENABLE
-   , CONSTRAINT pk_crvn_sec_objects PRIMARY KEY (cobj_id) ENABLE
-   , CONSTRAINT uk_crvn_sec_objects UNIQUE (cctx_id, cobj_name) ENABLE
-   , CONSTRAINT fk_crvnsecobjs_crvnsecctxs FOREIGN KEY (cctx_id) REFERENCES mydb.crvn_sec_contexts (cctx_id) ON DELETE CASCADE ENABLE
-);
-
-CREATE SEQUENCE mydb.crvn_sec_objects_id;
+CREATE TABLE IF NOT EXISTS `mydb`.`crvn_sec_objects` (
+  `cobj_id`           INT             NOT NULL AUTO_INCREMENT,
+  `cctx_id`           INT             NOT NULL,
+  `cobj_name`         VARCHAR(32)     NOT NULL,
+  `cobj_descr`        VARCHAR(256)    NOT NULL,
+  `cobj_type`         VARCHAR(8)      NOT NULL,
+  PRIMARY KEY (`cobj_id`),
+  UNIQUE INDEX `uk_crvn_sec_objects` (`cobj_name`(32) ASC, `cctx_id` ASC),
+  CONSTRAINT `fk_crvnsecobjs_crvnsecctxs`
+    FOREIGN KEY (`cctx_id`)
+    REFERENCES `mydb`.`crvn_sec_contexts` (`cctx_id`)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION);
