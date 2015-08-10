6367703f0cae38ef0289ad08243c01926d907782
Sara Viacava <sara82v@users.noreply.github.com>
2015-08-10 14:43:03 +0200

    ...

--- Caravan.ReportingService/Reports/Sample/Sample.aspx.cs
+++ Caravan.ReportingService/Reports/Sample/Sample.aspx.cs
@@ -5,16 +5,17 @@ namespace Finsa.Caravan.ReportingService.Reports.Sample
 {
     public partial class Sample : AbstractReportPage
     {
+       
         public Sample() : base("Sample Report", "~/Reports/Sample/Sample.rdlc")
         {
         }
 
         protected override void SetReportParameters(IList<ReportParameter> parameters)
-        {
+        {            
         }
 
         protected override void OnReportProcessing(ReportViewer reportViewer, SubreportProcessingEventArgs args)
-        {
+        {           
         }
     }
 }
