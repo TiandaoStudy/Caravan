1d0cee5d8404f866f72015dd07d8303ec5788128
Alessio Parma <alessio.parma@gmail.com>
2015-08-20 11:24:55 +0200

    working on logging...

---
+++ Caravan.Common/Logging/CaravanNLogLogger.cs
@@ -0,0 +1,14 @@
+﻿using Common.Logging.NLog;
+using NLog;
+using PommaLabs.KVLite;
+
+namespace Finsa.Caravan.Common.Logging
+{
+    public sealed class CaravanNLogLogger : NLogLogger
+    {
+        CaravanNLogLogger(Logger logger) : base(logger)
+        {
+            //MemoryCache.DefaultInstance;
+        }
+    }
+}
