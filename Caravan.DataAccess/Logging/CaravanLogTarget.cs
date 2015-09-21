7fd547ec8570a97720c36ef53b7690bdcff09e24
Alessio Parma <alessio.parma@gmail.com>
2015-09-21 23:45:09 +0200

    log async

--- Caravan.DataAccess/Logging/CaravanLogTarget.cs
+++ Caravan.DataAccess/Logging/CaravanLogTarget.cs
@@ -1,5 +1,4 @@
 ﻿using Finsa.Caravan.Common;
-using Finsa.Caravan.Common.Logging;
 using Finsa.Caravan.Common.Logging.Models;
 using Finsa.CodeServices.Common.Collections.ReadOnly;
 using NLog;
@@ -9,8 +8,7 @@ using NLog.Layouts;
 using NLog.Targets;
 using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
+using System.Runtime.CompilerServices;
 using LogLevel = Common.Logging.LogLevel;
 
 namespace Finsa.Caravan.DataAccess.Logging
@@ -19,18 +17,11 @@ namespace Finsa.Caravan.DataAccess.Logging
     ///   Target per il log di Caravan su database.
     /// </summary>
     [Target("CaravanLog")]
-    public class CaravanLogTarget : Target
+    public sealed class CaravanLogTarget : Target
     {
-        static readonly SimpleLayout DefaultLogLevel = new SimpleLayout("${level}");
-        static readonly SimpleLayout DefaultUserLogin = new SimpleLayout("${identity:name=true:lowercase=true}");
-        static readonly SimpleLayout DefaultCodeUnit = new SimpleLayout("${callsite:className=true:methodName=false:lowercase=true}");
-        static readonly SimpleLayout DefaultFunction = new SimpleLayout("${callsite:className=false:methodName=true:lowercase=true}");
-
-        /// <summary>
-        ///   Il layout da applicare per mostrare il livello di log.
-        /// </summary>
-        [RequiredParameter]
-        public Layout LogLevel { get; set; } = DefaultLogLevel;
+        private static readonly SimpleLayout DefaultUserLogin = new SimpleLayout("${identity:name=true:lowercase=true}");
+        private static readonly SimpleLayout DefaultCodeUnit = new SimpleLayout("${callsite:className=true:methodName=false:lowercase=true}");
+        private static readonly SimpleLayout DefaultFunction = new SimpleLayout("${callsite:className=false:methodName=true:lowercase=true}");
 
         /// <summary>
         ///   Il layout da applicare per mostrare l'utente loggato che ha prodotto il messaggio.
@@ -73,23 +64,15 @@ namespace Finsa.Caravan.DataAccess.Logging
         {
             try
             {
-                var logLevel = (LogLevel) Enum.Parse(typeof(LogLevel), LogLevel.Render(logEvent));
-                var userLogin = UserLogin.Render(logEvent);
-                var codeUnit = CodeUnit.Render(logEvent);
-                var function = Function.Render(logEvent);
-
-                var logMessage = ParseMessage(logEvent);
-                logMessage.ShortMessage = (ShortMessage != null) ? ShortMessage.Render(logEvent) : logMessage.ShortMessage;
-                logMessage.LongMessage = (LongMessage != null) ? LongMessage.Render(logEvent) : logMessage.LongMessage;
-                logMessage.Context = (Context != null) ? Context.Render(logEvent) : logMessage.Context;          
+                var logMessage = ToLogMessage(logEvent);
 
                 // In order to be able to use thread local information, it must _not_ be async.
                 var result = CaravanDataSource.Logger.LogRaw(
-                    logLevel,
+                    ParseLogLevel(logEvent.Level),
                     CommonConfiguration.Instance.AppName,
-                    userLogin,
-                    codeUnit,
-                    function,
+                    UserLogin.Render(logEvent),
+                    CodeUnit.Render(logEvent),
+                    Function.Render(logEvent),
                     logMessage.ShortMessage,
                     logMessage.LongMessage,
                     logMessage.Context,
@@ -103,28 +86,24 @@ namespace Finsa.Caravan.DataAccess.Logging
             }
             catch (Exception ex)
             {
-                // Uso il log di emergenza, nel caso ci siano stati errori.
-                try
-                {
-                    // Devo loggare immediatamente l'eccezione che è stata ricevuta.
-                    // Cerco di salvare comunque il messaggio di log appena emesso.
-                    ServiceProvider.EmergencyLog.Error($"Internal error while logging [{logEvent.FormattedMessage}]", ex);
-                }
-                catch
-                {
-                    // Se anche qui da errore, mi arrendo :(
-                }
+                // Uso il log di emergenza nel caso ci siano stati errori.
+                UseEmergencyLog(ex, logEvent.FormattedMessage);
             }
         }
 
         protected override void Write(AsyncLogEventInfo logEvent)
         {
-            base.Write(logEvent);
+            Write(logEvent.LogEvent);
+            logEvent.Continuation?.Invoke(null);
         }
 
         protected override void Write(AsyncLogEventInfo[] logEvents)
         {
-            base.Write(logEvents);
+            foreach (var logEvent in logEvents)
+            {
+                Write(logEvent.LogEvent);
+                logEvent.Continuation?.Invoke(null);
+            }
         }
 
         protected override void FlushAsync(AsyncContinuation asyncContinuation)
@@ -132,12 +111,8 @@ namespace Finsa.Caravan.DataAccess.Logging
             asyncContinuation?.Invoke(null);
         }
 
-        static void EmptyAsyncContinuation(Exception ex)
-        {
-            // Nulla di che, per ora...
-        }
-
-        static LogMessage ParseMessage(LogEventInfo logEvent)
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private LogMessage ToLogMessage(LogEventInfo logEvent)
         {
             // Il messaggio che è giunto nel log: può essere una stringa semplice, oppure uno YAML
             // di LogMessage.
@@ -169,8 +144,59 @@ namespace Finsa.Caravan.DataAccess.Logging
                 logMessage.Exception = exception;
             }
 
-            // Restituisco il messaggio appena elaborato.
+            logMessage.ShortMessage = ShortMessage?.Render(logEvent) ?? logMessage.ShortMessage;
+            logMessage.LongMessage = LongMessage?.Render(logEvent) ?? logMessage.LongMessage;
+            logMessage.Context = Context?.Render(logEvent) ?? logMessage.Context;
+
             return logMessage;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static LogLevel ParseLogLevel(NLog.LogLevel logLevel)
+        {
+            switch (logLevel.Name[0])
+            {
+                case 'd':
+                case 'D':
+                    return LogLevel.Debug;
+
+                case 't':
+                case 'T':
+                    return LogLevel.Trace;
+
+                case 'i':
+                case 'I':
+                    return LogLevel.Info;
+
+                case 'w':
+                case 'W':
+                    return LogLevel.Warn;
+
+                case 'e':
+                case 'E':
+                    return LogLevel.Error;
+
+                case 'f':
+                case 'F':
+                    return LogLevel.Fatal;
+
+                default:
+                    return LogLevel.Debug;
+            }
+        }
+
+        private static void UseEmergencyLog(Exception ex, string logMessage)
+        {
+            try
+            {
+                // Devo loggare immediatamente l'eccezione che è stata ricevuta.
+                // Cerco di salvare comunque il messaggio di log appena emesso.
+                ServiceProvider.EmergencyLog.Error($"Internal error while logging [{logMessage}]", ex);
+            }
+            catch
+            {
+                // Se anche qui da errore, mi arrendo :(
+            }
         }
     }
 }
