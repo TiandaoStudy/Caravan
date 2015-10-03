1d7673d071daf7f999b28cc3b4804741f6d9f2bd
Alessio Parma <alessio.parma@gmail.com>
2015-10-03 08:52:47 +0200

    configuring identity...

--- Caravan.DataAccess/Drivers/Sql/Identity/SqlIdnServiceFactory.cs
+++ Caravan.DataAccess/Drivers/Sql/Identity/SqlIdnServiceFactory.cs
@@ -1,22 +1,44 @@
-﻿using Finsa.Caravan.DataAccess.Drivers.Sql.Identity.Services;
+﻿using Finsa.Caravan.Common;
+using Finsa.Caravan.DataAccess.Drivers.Sql.Identity.Services;
 using Finsa.Caravan.DataAccess.Drivers.Sql.Identity.Stores;
+using Finsa.CodeServices.Clock;
 using IdentityServer3.Core.Configuration;
 using IdentityServer3.Core.Services;
+using Ninject;
 
 namespace Finsa.Caravan.DataAccess.Drivers.Sql.Identity
 {
+    /// <summary>
+    ///   Configura IdentityServer in modo da usare il driver SQL per l'accesso ai dati.
+    /// </summary>
     public static class SqlIdnServiceFactory
     {
+        /// <summary>
+        ///   Configura IdentityServer in modo da usare il driver SQL per l'accesso ai dati.
+        /// </summary>
         public static IdentityServerServiceFactory Configure()
         {
             var factory = new IdentityServerServiceFactory
             {
+                // Operational stores
                 AuthorizationCodeStore = new Registration<IAuthorizationCodeStore, SqlIdnAuthorizationCodeStore>(),
                 TokenHandleStore = new Registration<ITokenHandleStore, SqlIdnTokenHandleStore>(),
                 ConsentStore = new Registration<IConsentStore, SqlIdnConsentStore>(),
                 RefreshTokenStore = new Registration<IRefreshTokenStore, SqlIdnRefreshTokenStore>(),
+
+                // Client configuration
+                ClientStore = new Registration<IClientStore, SqlIdnClientStore>(),
+
+                // Scope configuration
+                ScopeStore = new Registration<IScopeStore, SqlIdnScopeStore>(),
+
+                // CORS handling
                 CorsPolicyService = new Registration<ICorsPolicyService>(r => new SqlIdnClientConfigurationCorsPolicyService(r.Resolve<SqlDbContext>()))
             };
+
+            // Further services registrations...
+            factory.Register(new Registration<SqlDbContext>(r => SqlDbContext.CreateWriteContext()));
+            factory.Register(new Registration<IClock>(r => CaravanServiceProvider.NinjectKernel.Get<IClock>()));
 
             return factory;
         }
