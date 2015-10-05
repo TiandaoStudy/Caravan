8893cb9106b708deb809c8dab4674bc8f67b88fc
Alessio Parma <alessio.parma@gmail.com>
2015-10-05 16:58:40 +0200

    script oracle...

---
+++ Caravan.DataAccess/Drivers/Sql/Oracle/Scripts/Triggers/ti_crvn_idn_consents.sql
@@ -0,0 +1,11 @@
+﻿-- REPLACE 'mydb' WITH DB NAME
+
+CREATE OR REPLACE TRIGGER mydb.ti_crvn_idn_consents
+BEFORE INSERT ON mydb.crvn_idn_consents 
+FOR EACH ROW
+BEGIN
+  SELECT mydb.pck_caravan_utils.f_get_sysdate_utc, mydb.pck_caravan_utils.f_get_sysuser, NULL, NULL
+    INTO :new.TRCK_INSERT_DATE, :new.TRCK_INSERT_DB_USER, :new.TRCK_UPDATE_DATE, :new.TRCK_UPDATE_DB_USER
+    FROM DUAL;
+END;
+/
