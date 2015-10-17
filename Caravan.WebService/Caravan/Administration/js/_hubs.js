d7fc11a05f1af5ecd399f65fdde45f353b8fd942
Alessio Parma <alessio.parma@gmail.com>
2015-10-17 09:34:32 +0200

    esempio per signalr

---
+++ Caravan.WebService/Caravan/Administration/js/_hubs.js
@@ -0,0 +1,14 @@
+﻿$(function () {
+    var ticker = $.connection.serverTime;
+    // Add a client-side hub method that the server will call
+    ticker.client.getServerTime = function (utcNow) {
+        console.log("UTCNOW: " + utcNow.UtcNow);
+    }
+    // Start the connection
+    $.connection.hub.logging = true;
+    $.connection.hub.start({ transport: ["serverSentEvents", "foreverFrame", "longPolling"] }).done(function () {
+        console.log("DONE");
+    }).fail(function () {
+        console.log("FAIL");
+    });
+});
---
+++ Caravan.WebService/Models/SignalR/ServerUtcNow.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Finsa.Caravan.WebService.Models.SignalR
+{
+    public sealed class ServerUtcNow
+    {
+        public DateTime UtcNow { get; set; }
+    }
+}
