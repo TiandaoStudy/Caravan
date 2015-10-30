f52fa9ba1ade325c3676ad361d478469b0acb479
Alessio Parma <alessio.parma@gmail.com>
2015-10-30 23:31:04 +0100

    pkg...

--- Caravan.WebApi/Folders/AuthorizeForCaravanAttribute.cs
+++ Caravan.WebApi/Folders/AuthorizeForCaravanAttribute.cs
@@ -1,19 +1,47 @@
-﻿using System.Threading;
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Controllers;
 using System.Web.Http.Filters;
+using Finsa.Caravan.Common.Logging;
+using PommaLabs.Thrower;
 
 namespace Finsa.Caravan.WebApi.Folders
 {
-    public sealed class AuthorizeForCaravanAttribute : ExceptionFilterAttribute
+    public sealed class AuthorizeForCaravanAttribute : ActionFilterAttribute
     {
-        public override async void OnException(HttpActionExecutedContext actionExecutedContext)
+        private readonly ICaravanLog _log;
+
+        public AuthorizeForCaravanAttribute(ICaravanLog log)
         {
-            await OnExceptionAsync(actionExecutedContext, CancellationToken.None);
+            RaiseArgumentNullException.IfIsNull(log, nameof(log));
+            _log = log;
         }
 
-        public override Task OnExceptionAsync(HttpActionExecutedContext actionExecutedContext, CancellationToken cancellationToken)
+        public static Func<HttpActionContext, CancellationToken, ICaravanLog, Task<bool>> AuthorizationGranted { get; set; } = (actionContext, cancellationToken, log) =>
         {
-            return base.OnExceptionAsync(actionExecutedContext, cancellationToken);
+            const string className = nameof(AuthorizeForCaravanAttribute);
+            const string propName = nameof(AuthorizationGranted);
+            log.Warn($"Caravan actions are disabled by default, you can enabled them by changing {className}.{propName}");
+            return Task.FromResult(false);
+        }; 
+
+        public override async void OnActionExecuting(HttpActionContext actionContext)
+        {
+            await OnActionExecutingAsync(actionContext, CancellationToken.None);
+        }
+
+        public override async Task OnActionExecutingAsync(HttpActionContext actionContext, CancellationToken cancellationToken)
+        {
+            if (await AuthorizationGranted(actionContext, cancellationToken, _log))
+            {
+                return;
+            }
+            _log.Warn("");
+            throw new HttpResponseException(HttpStatusCode.Unauthorized);
         }
     }
 }
