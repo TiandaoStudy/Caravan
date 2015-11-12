f1ed19af4153b643674fc4a81591be1f7559b993
Alessio Parma <alessio.parma@gmail.com>
2015-11-12 12:44:08 +0100

    ...

--- Caravan.DataAccess/Sql/Oracle/Scripts/Tables/crvn_sec_roles.sql
+++ Caravan.DataAccess/Sql/Oracle/Scripts/Tables/crvn_sec_roles.sql
@@ -4,6 +4,7 @@ CREATE TABLE mydb.crvn_sec_roles
 (
      crol_id          NUMBER          NOT NULL
    , capp_id          NUMBER          NOT NULL 
+   , cgrp_id          NUMBER          NOT NULL 
    , crol_name        NVARCHAR2(32)   NOT NULL
    , crol_descr       NVARCHAR2(256)  NOT NULL
    , crol_notes       NVARCHAR2(1024) NOT NULL
@@ -11,6 +12,7 @@ CREATE TABLE mydb.crvn_sec_roles
    , CONSTRAINT pk_crvn_sec_roles PRIMARY KEY (crol_id) ENABLE
    , CONSTRAINT uk_crvn_sec_roles UNIQUE (crol_name, capp_id) ENABLE
    , CONSTRAINT fk_crvnsecroles_crvnsecapps FOREIGN KEY (capp_id) REFERENCES mydb.crvn_sec_apps (capp_id) ON DELETE CASCADE ENABLE
+   , CONSTRAINT fk_crvnsecroles_crvnsecgrps FOREIGN KEY (cgrp_id) REFERENCES mydb.crvn_sec_groups (cgrp_id) ON DELETE CASCADE ENABLE
 );
 
 CREATE SEQUENCE mydb.crvn_sec_roles_id NOCACHE;
