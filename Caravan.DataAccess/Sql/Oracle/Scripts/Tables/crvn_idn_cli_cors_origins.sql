5a3acdeae343cfe6ad7add72be7a529bc1a70199
Alessio Parma <alessio.parma@gmail.com>
2015-11-13 10:15:49 +0100

    ... oracle

--- Caravan.DataAccess/Sql/Oracle/Scripts/Tables/crvn_idn_cli_cors_origins.sql
+++ Caravan.DataAccess/Sql/Oracle/Scripts/Tables/crvn_idn_cli_cors_origins.sql
@@ -6,7 +6,7 @@ CREATE TABLE mydb.crvn_idn_cli_cors_origins
    , CCLI_ID                NUMBER(10)      NOT NULL
    , CCCO_ORIGIN            NVARCHAR2(150)  NOT NULL
 
-   , CONSTRAINT pk_crvn_idn_cli_cors_origins PRIMARY KEY (CCLI_ID) ENABLE
+   , CONSTRAINT pk_crvn_idn_cli_cors_origins PRIMARY KEY (CCCO_ID) ENABLE
    , CONSTRAINT fk_crvnidn_clicorsorig_clients FOREIGN KEY (CCLI_ID) REFERENCES mydb.crvn_idn_clients (CCLI_ID) ON DELETE CASCADE ENABLE
 );
 
