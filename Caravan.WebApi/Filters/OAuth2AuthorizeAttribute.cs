0bca77f25cd32c5175b3b6f9b64ca6caddc8b2cc
Alessio Parma <alessio.parma@gmail.com>
2015-12-07 17:44:27 +0100

    oauth2

---
+++ Caravan.WebApi/Filters/OAuth2AuthorizeAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finsa.Caravan.WebApi.Filters
+{
+    class OAuth2AuthorizeAttribute
+    {
+    }
+}
