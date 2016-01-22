1cac7e8d97d0dc04aa2601b245f847ff1b7716dc
Alessio Parma <alessio.parma@gmail.com>
2016-01-22 16:09:37 +0100

    cleanup

--- Caravan.Worker/Program.cs
+++ Caravan.Worker/Program.cs
@@ -1,9 +1,4 @@
 ﻿using Finsa.Caravan.Common.Logging.Workflows;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Caravan.Worker
 {
